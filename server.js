69ee62c01f2d19e18706afdeb66085eef0dc9931
Romain Lemoine-Melin <20278@ecam.be>
2024-11-14 18:57:33 +0100

    Bugfixes

--- server.js
+++ server.js
@@ -3,59 +3,57 @@ import Word from './models/Word.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-let message = false;
-let word = NaN;
-let words = NaN;
 
-app.post("/manager/add", async function (req, res) {
-  const word = new Word();
-  word.word = req.body.word;
-  word.nAttempts = 0;
-  word.nCorrects = 0;
-  word.correctWord = req.body.translation;
+app.post("/manager/add", async (req, res) => {
+  console.log(req.body);
+  console.log(req.body.word);
+  console.log(req.body.correctWord);
+  const word = new Word({
+    word: req.body.word,
+    nAttempts: 0,
+    nCorrects: 0,
+    correctWord: req.body.correctWord
+  });
+  await word.update({ word: req.body.word, nAttempts: 0, nCorrects: 0, correctWord: req.body.correctWord });
+  console.log(word);
   await word.save();
   res.redirect('/manager');
 });
 
-app.get("/manager", async function (req, res) {
+app.get("/manager", async (req, res) => {
+  const message = req.query.message;
   const words = await Word.loadMany();
-  let scores = [];
-  for (var i = 0; i < words.length; i++) {
-    let score = "0%"
-    if (words[i].nAttempts != 0) {
-        score = toString(words[i].nCorrects / words[i].nAttempts * 100);
-    } 
-    scores.push(score);
-  }
-  res.render("screen2.ejs", {message : message, scores : scores, words: words});
-})
+  const scores = words.map(w => w.nAttempts ? `${(w.nCorrects / w.nAttempts * 100).toFixed(2)}%` : "0%");
+  res.render("screen2.ejs", { message, scores, words });
+});
 
-app.get("/manager/delete/:id", async function (req, res) {
-  await Word.delete({ idwords: req.params.id });
-  console.log(req.params)
+app.get("/manager/delete/:id", async (req, res) => {
+  await Word.delete({ id: req.params.id });
   res.redirect('/manager');
 });
 
-app.post("/correct", async function (req, res) {
-  let test = req.body.input
-  console.log(word);
-  if (test == word.correctWord) {
-    message = "You succeeded !"
+app.post("/correct", async (req, res) => {
+  console.log(req.body);
+  const test = req.body.input;
+  let word = await Word.load({ correctWord: req.body.correctWord });
+  let message;
+  if (test === word.correctWord) {
+    message = "You succeeded!";
     word.nCorrects++;
-    res.redirect('/')
-  }
-  else {
-    message = "Wrong answer. Try again"
+  } else {
+    message = "Wrong answer. Try again";
   }
   word.nAttempts++;
+  word.update({ nAttempts: word.nAttempts, nCorrects: word.nCorrects });
   await word.save();
-})
+  res.redirect(`/?message=${encodeURIComponent(message)}`);
+});
 
-app.get("/", async function (req, res) {
-  words = await Word.loadMany();
-  const random = Math.floor(Math.random()*words.length)
-  word = words[random];
-  res.render('screen1.ejs', { word: word, message: message });
+app.get("/", async (req, res) => {
+  const words = await Word.loadMany();
+  const word = words[Math.floor(Math.random() * words.length)];
+  const message = req.query.message;
+  res.render('screen1.ejs', { word, message });
 });
 
 app.listen(4000);
